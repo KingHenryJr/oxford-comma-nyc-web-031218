f64f5b70aa31e45bb41f41d49faf99ad6daac85b
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 23:26:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length === 2 
     return array.join(" and ")
   elsif array.length >= 3
-    return 
-  
-  
+    create a new last 
+    pop last off
+  add 
 end
