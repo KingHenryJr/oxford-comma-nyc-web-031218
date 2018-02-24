e00d343d81a765b73fb83b578773b34fbb70172d
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 23:28:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,5 @@ def oxford_comma(array)
   elsif array.length === 2 
     return array.join(" and ")
   elsif array.length >= 3
-    create a new last 
-    pop last off
-  add 
+    array[-1] = "and ${}"
 end
